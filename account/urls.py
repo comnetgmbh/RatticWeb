95c8d88388a00b87b813cbda0e87b306941eb122
Simon Baumgarten <simon.baumgarten@comnetgmbh.com>
2019-11-21 09:52:21 -0500

    changed change_password to new django password class

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import include, url
+from django.shortcuts import redirect
+from django.urls import path, reverse_lazy
 from django.conf import settings
 
 from .views import profile, newapikey, deleteapikey, RatticSessionDeleteView
@@ -11,62 +13,58 @@ from two_factor.views import QRGeneratorView
 import django
 import account
 
-from django.contrib.auth.views import LogoutView as logout
+
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
-    url(r'^$', profile, {}),
-    url(r'^newapikey/$', newapikey, {}),
-    url(r'^deleteapikey/(?P<key_id>\d+)/$', deleteapikey, {}),
+    path('', profile, name='profile'),
+    path('newapikey/', newapikey, name='newapikey'),
+    path('deleteapikey/<int:key_id>/', deleteapikey, name='deleteapikey'),
 
-    url('logout/', logout.get_next_page,name='logout'),
+    path('logout/', auth_views.LogoutView.get_next_page,name='logout'),
 
     # View to kill other sessions with
-    url(r'^killsession/(?P<pk>\w+)/', RatticSessionDeleteView.as_view(), name='kill_session'),
+    path('killsession/<str:pk>/', RatticSessionDeleteView.as_view(), name='kill_session'),
 
     # Two Factor Views
-    url(r'^login/$', RatticTFALoginView.as_view(), name='login'),
-    url(r'^generate_api_key$', RatticTFAGenerateApiKey.as_view(), name="generate_api_key"),
+    path('login/', RatticTFALoginView.as_view(), name='login'),
+    path('generate_api_key', RatticTFAGenerateApiKey.as_view(), name="generate_api_key"),
 
-    url(r'^two_factor/disable/$', RatticTFADisableView.as_view(), name='tfa_disable'),
-    url(r'^two_factor/backup/$', RatticTFABackupTokensView.as_view(), name='tfa_backup'),
-    url(r'^two_factor/setup/$', RatticTFASetupView.as_view(), name='tfa_setup'),
-    url(r'^two_factor/qr/$', QRGeneratorView.as_view(), name='tfa_qr'),
+    path('two_factor/disable/', RatticTFADisableView.as_view(), name='tfa_disable'),
+    path('two_factor/backup/', RatticTFABackupTokensView.as_view(), name='tfa_backup'),
+    path('two_factor/setup/', RatticTFASetupView.as_view(), name='tfa_setup'),
+    path('two_factor/qr/', QRGeneratorView.as_view(), name='tfa_qr'),
 ]
 
 if settings.GOAUTH2_ENABLED:
     urlpatterns += [
-        url(r'', include('social_auth.urls')),
+        path('', include('social_auth.urls')),
     ]
 
 # URLs we don't want enabled with LDAP
 if not settings.LDAP_ENABLED:
     urlpatterns += [
-        url(r'^reset/$', django.contrib.auth.views.PasswordResetView,
-            {
-                'post_reset_redirect': '/account/reset/done/',
-                'template_name': 'password_reset.html'
-            },
+        path('reset/',
+            auth_views.PasswordResetView.as_view(template_name='password_reset.html', success_url='/account/reset/done/'),
             name="password_reset"
-        ),
+            ),
 
-        url(r'^reset/done/$', django.contrib.auth.views.PasswordResetDoneView, {
-            'template_name': 'password_reset_done.html'},
+        path('reset/done/',
+            auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
             name="password_reset_done"
-        ),
+            ),
 
-        url(r'^reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', django.contrib.auth.views.PasswordResetConfirmView, {
-            'post_reset_redirect': '/',
-            'template_name': 'password_reset_confirm.html'},
+        path('reset/<str:uidb64>-<str:token>)/',
+            auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html', success_url='/'),
             name="password_reset_confirm"
-        ),
+            ),
 
-        url(r'^changepass/$', account.views.rattic_change_password, {
-            'post_change_redirect': '/account/',
-            'template_name': 'account_changepass.html'}, name='password_change')
+        path('changepass/', auth_views.PasswordChangeView.as_view(template_name='account_changepass.html', success_url=reverse_lazy('profile') ), name='password_change')
     ]
 
 # URLs we do want enabled with LDAP
 if settings.LDAP_ENABLED and settings.AUTH_LDAP_ALLOW_PASSWORD_CHANGE:
     urlpatterns += [
-        url(r'^changepass/$', account.views.ldap_password_change, {}, name='password_change')
+        path('changepass/', account.views.ldap_password_change, {}, name='password_change')
     ]
